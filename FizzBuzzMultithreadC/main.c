f3ec61490c704c4a65f604676939f9f5b514df65
Michael Bernd Beier <code@michaelberndbeier.com>
2021-10-30 08:57:02 +0200

    Initial Commit

---
+++ FizzBuzzMultithreadC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FizzBuzzMultithreadC
+//
+//  Created by mbb on 30.10.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
